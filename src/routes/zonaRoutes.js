5b0c36c0ed534b8e631efd60d581079f648aadb1
MaxiVigetti <mmvigetti@alumnos.unsada.edu.ar>
2025-11-13 20:22:30 -0300

    agregar delito desde caso,gestionar delitos

--- src/routes/zonaRoutes.js
+++ src/routes/zonaRoutes.js
@@ -4,6 +4,7 @@ import { listarZonas, agregarZona, editarZona, borrarZona} from "../controllers/
 const router = express.Router();
 
 // Listar todas las zonas (si lo necesitás)
+/*router.get("/", listarZonas);*/
 router.get("/", listarZonas);
 
 // Agregar una nueva zona (llamado por el modal con fetch)
