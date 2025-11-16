b62861090dec16b7412fb7fdcc5b8d0d80452ea6
byariel <aromero@alumnos.unsada.edu.ar>
2025-11-16 20:13:35 -0300

    Feat agregaron gestionar tipos de casos , alertas etc

--- src/routes/personaRoutes.js
+++ src/routes/personaRoutes.js
@@ -20,39 +20,8 @@ router.post("/agregar-persona", protect, crearPersona);
 // APIs para el modal
 router.get("/personas", protect, listarPersonas);
 
+
 router.put("/personas/:id", protect, actualizarPersona);
-
-
-
-
-
-
-
-
-
-
-/*
-router.get("/agregar-persona", mostrarFormularioPersona);
-router.post("/agregar-persona", crearPersona);
-
-router.get("/personas", listarPersonas);  // GET /personas?search=texto (lista con búsqueda)
-router.delete("/personas/:id", borrarPersona);  // DELETE /personas/:id (borrar)
-router.put("/personas/:id", actualizarPersona);  // PUT /personas/:id (editar)
-*/
-
-/*
-// Ruta para mostrar formulario de agregar persona
-router.get('/agregar-persona', protect, async (req, res) => {
-  try {
-    const personas = await obtenerPersonas(); // Función que trae datos desde DB
-    res.render('agregar-persona', {
-      user: req.user,       // Contiene id_rol, nombre, etc.
-      personas: JSON.stringify(personas)// Lista para selects o tablas
-    });
-  } catch (error) {
-    console.error('Error al cargar formulario:', error);
-    res.status(500).render('error', { message: 'Error al cargar el formulario' });
-  }
-});*/
+router.delete("/borrarPersona/:id", protect, borrarPersona);
 
 export default router;
